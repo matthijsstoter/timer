91bacdd10dd5872ade8703ec7c20e9afcfacb0ee
matthijsstoter <matthijsstoter@hetnet.nl>
2022-04-29 22:13:16 +0200

    Changed to perfcounter to improve performance

--- timer.py
+++ timer.py
@@ -1,12 +1,12 @@
-from time import time
+from time import perf_counter
 from typing import Callable
 
 
 def timer(func: Callable) -> Callable:
     def f(*args, **kwargs):
-        before = time()
+        before = perf_counter()
         rv = func(*args, **kwargs)
-        after = time()
+        after = perf_counter()
         print(f"Time elapsed {after-before} s")
         return rv
     return f
